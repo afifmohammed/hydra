4ee9f881d3bb709ffaac385d0824663661e77982
afifmohammed <afifmohammed@gmail.com>
2016-03-27 00:11:27 +1100

    house keeping

--- EventSourcing/Correlations.cs
+++ EventSourcing/Correlations.cs
@@ -8,23 +8,29 @@ namespace EventSourcing
 {
     public struct CorrelationMap
     {
-        public static CorrelationMap Between<THandlerData, TNotification>(
-            Expression<Func<THandlerData, object>> handlerDataProperty,
-            Expression<Func<TNotification, object>> notificationProperty)
-        {
-            return new CorrelationMap
-            {
-                HandlerDataContract = typeof(THandlerData).Contract(),
-                NotificationContract = typeof(TNotification).Contract(),
-                NotificationPropertyName = notificationProperty.GetPropertyName(),
-                HandlerDataPropertyName = handlerDataProperty.GetPropertyName()
-            };
-        }
-
         public TypeContract HandlerDataContract { get; set; }
         public string HandlerDataPropertyName { get; set; }
         public TypeContract NotificationContract { get; set; }
         public string NotificationPropertyName { get; set; }
+    }
+
+    public struct Correlation       
+    {
+        public TypeContract Contract { get; set; }
+        public string PropertyName { get; set; }
+        public Lazy<string> PropertyValue { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Correlation))
+                return false;
+
+            var other = (Correlation)obj;
+
+            return other.Contract.Equals(Contract)
+                && other.PropertyName == PropertyName
+                && other.PropertyValue.Value == PropertyValue.Value;
+        }
     }
 
     public static partial class Type<TSource> where TSource : new()
@@ -56,7 +62,7 @@ namespace EventSourcing
                     var contract = JsonConvert.DeserializeObject<TContract>(json.Value);
                     var source = d;
                     mapper(contract)(source);
-                    return source;                    
+                    return source;
                 }
             );
         }
@@ -76,23 +82,4 @@ namespace EventSourcing
             };
         }
     }
-
-    public struct Correlation       
-    {
-        public TypeContract Contract { get; set; }
-        public string PropertyName { get; set; }
-        public Lazy<string> PropertyValue { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is Correlation))
-                return false;
-
-            var other = (Correlation)obj;
-
-            return other.Contract.Equals(Contract)
-                && other.PropertyName == PropertyName
-                && other.PropertyValue.Value == PropertyValue.Value;
-        }
-    }  
 }
