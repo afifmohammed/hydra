ca8fae0dc980a007124ca59ad9be6c2390239818
Afif Mohammed <askafif@y7mail.com>
2016-03-29 11:21:08 +1100

    Moved fluent api and use case builder into separate project from core

---
+++ Api/FluentInterfaces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EventSourcing
+{
+    public interface CorrelationMap<TData, TNotification> : Given<TData>, When<TData>, Then<TData, TNotification>
+        where TData : new()
+    {
+        CorrelationMap<TData, TNotification> Correlate(Expression<Func<TNotification, object>> left, Expression<Func<TData, object>> right);
+    }
+
+    public interface Given<TData>
+        where TData : new()
+    {
+        CorrelationMap<TData, TNotification> Given<TNotification>(Func<TNotification, Action<TData>> mapper);
+    }
+
+    public interface When<TData> where TData : new()
+    {
+        CorrelationMap<TData, TNotification> When<TNotification>(Func<TNotification, Action<TData>> mapper);
+    }
+
+    public interface Then<TData, out TNotification> : When<TData>
+        where TData : new()
+    {
+        When<TData> Then(Func<TData, TNotification, IEnumerable<IDomainEvent>> handler);
+    }
+}
---
+++ Api/UseCaseBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EventSourcing
+{
+    public class UseCase<TData> : Given<TData>
+        where TData : new()
+    {
+        public CorrelationMap<TData, TNotification> Given<TNotification>(Func<TNotification, Action<TData>> mapper)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Then<TData, TNotification> When<TNotification>(Func<TNotification, Action<TData>> mapper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
