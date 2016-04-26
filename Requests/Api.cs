8b2ed9a7fe28f7446260b2f73daa5c270c616e10
Afif Mohammed <askafif@y7mail.com>
2016-04-26 12:18:07 +1000

    Opened up the request API for support for object as request

--- Requests/Api.cs
+++ Requests/Api.cs
@@ -4,22 +4,29 @@ using System.Linq;
 
 namespace Requests
 {
-    public interface IRequest<TResult> where TResult : class
-    {}
-
-    public delegate IEnumerable<TResult> RequestHandler<TResult>(IRequest<TResult> input) where TResult : class;
+    public delegate IEnumerable<TResult> RequestHandler<out TResult>(object request) where TResult : class;
     
     public static class Request<TResult> where TResult : class
     {
-        public static RequestHandler<TResult> By = 
-            input => RequestHandlers.Routes
-                .Where(r => r.Key.Equals(Contract(input)))
-                .SelectMany(r => ((Func<object, IEnumerable<TResult>>)(r.Value))(input));
+        public static RequestHandler<TResult> By =
+            request => RequestHandlers.Routes
+                .Where(r => r.Key.Equals(Contract(request)))
+                .SelectMany(r => ((Func<object, IEnumerable<TResult>>)(r.Value))(request));
 
-        static FunctionContract Contract(IRequest<TResult> input)
+        static FunctionContract Contract(object request)
         {
-            return new FunctionContract(input.Contract(), typeof(TResult).Contract());
+            return new FunctionContract(request.Contract(), typeof(TResult).Contract());
         }        
     }
 
+    public interface IRequest<TResult> where TResult : class
+    { }
+
+    public static class Request
+    {
+        public static IEnumerable<TResult> For<TResult>(IRequest<TResult> request) where TResult : class
+        {
+            return Request<TResult>.By(request);
+        }
+    }
 }
