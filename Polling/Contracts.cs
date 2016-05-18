e4e045f286c2b76f817b010674eb0586e75784bc
afifmohammed <afifmohammed@gmail.com>
2016-05-18 18:07:04 +1000

    first cut polling

---
+++ Polling/Contracts.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using EventSourcing;
+
+namespace Polling
+{
+    public class EventId : Unit<long>
+    {
+        public long Value
+        {
+            get;set;
+        }
+    }
+
+    public class EventName : Unit<string>
+    {
+        public string Value
+        {
+            get;set;
+        }
+    }
+
+    public class Notification
+    {
+        public EventId Id { get; set; }
+        public IDomainEvent Event { get; set; }
+    }
+
+    public delegate IEnumerable<Notification> RecentNotifications(EventId id, EventName[] names);    
+
+    public delegate void RecordLastSeen(EventId id);
+
+    public delegate EventId LastSeen();    
+}
---
+++ Polling/Functions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventSourcing;
+
+namespace Polling
+{
+    public static class Functions
+    {
+        public static LastSeen Apply(
+            LastSeen lastSeen,
+            RecentNotifications recentNotifications,
+            IEnumerable<TypeContract> contracts,
+            Action<IEnumerable<IDomainEvent>> publish)
+        {
+            return () => Apply
+            (
+                recentNotifications
+                (
+                    lastSeen(),
+                    contracts.Select(x => new EventName { Value = x.Value }).ToArray()
+                ),
+                publish
+            );
+        }
+
+        static EventId Apply(
+            IEnumerable<Notification> notifications,
+            Action<IEnumerable<IDomainEvent>> publish)
+        {
+            publish(notifications.Select(x => x.Event));
+            return notifications.Reverse().First().Id;
+        }
+    }
+}
