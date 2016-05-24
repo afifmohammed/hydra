0dc93e2aa680431d95c471cb052eddda2e054278
Afif Mohammed <askafif@y7mail.com>
2016-05-24 17:08:06 +1000

    schema for subscriptions

---
+++ AdoNet/SubscriptionStore.sql
@@ -0,0 +1,11 @@
+﻿USE [SubscriptionStore]
+
+CREATE TABLE [dbo].[Subscriptions](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[NotificationContract] [nvarchar](250) NOT NULL,
+	[SubscriberContract] [nvarchar](250) NOT NULL,
+	CONSTRAINT [PK_Subscriptions] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	) ON [PRIMARY]
+) ON [PRIMARY]
